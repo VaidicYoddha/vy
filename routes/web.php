ab30ea02983d1fe2b76755a45c21d3186f1ca8bf
VaidicYoddha <103875498+VaidicYoddha@users.noreply.github.com>
2023-01-20 19:44:11 +0530

    vy15

--- routes/web.php
+++ routes/web.php
@@ -93,7 +93,4 @@ Route::get('/shanka-samadhan/search', [ShankaController::class, 'search'])->name
 Auth::routes();
 
 
-
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
